ce88eae35e341737dc9e92a43e1a5586770371dc
Examen-Gabriel-Arroyo <>
2025-03-17 11:20:32 -0600

    commit 7

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 var nombre = "Gabriel Arroyo";
 console.log("Hola, mi nombre es " + nombre);
 
-const mediaQuery = window.matchMedia('(max-width: 768px)');
-
-function manejarCambio(e) {
-    if (e.matches) {
-        console.log('Vista móvil');
-    } else {
-        console.log('Vista escritorio');
-    }
-}
-
-mediaQuery.addListener(manejarCambio);
-manejarCambio(mediaQuery); // Ejecutar una vez al cargar
